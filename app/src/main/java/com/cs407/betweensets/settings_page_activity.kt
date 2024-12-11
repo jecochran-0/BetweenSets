46afac83e038afaa475c2ff05b577dff35b822f7
AlGilley <158014075+AlGilley@users.noreply.github.com>
2024-12-10 19:39:19 -0600

    Updated settings to show user info

--- app/src/main/java/com/cs407/betweensets/settings_page_activity.kt
+++ app/src/main/java/com/cs407/betweensets/settings_page_activity.kt
@@ -8,22 +8,51 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.content.Context
+import android.content.SharedPreferences
+import android.widget.TextView
+import android.widget.Toast
 
 class settings_page_activity : AppCompatActivity() {
 
+    private lateinit var sharedPreferences: SharedPreferences
+    private lateinit var activeUsernameTextView: TextView
+    private lateinit var activePasswordTextView: TextView
+    private lateinit var userViewModel: UserViewModel
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_settings_page)
 
+
+        sharedPreferences = getSharedPreferences("UserPrefs", Context.MODE_PRIVATE)
+
+
+        activeUsernameTextView = findViewById(R.id.activeUsername2)
+        activePasswordTextView = findViewById(R.id.activePassword)
+        userViewModel = (application as ViewModelExtend).userViewModel
+
+
+        setUserData()
     }
-  //  override fun onCreateOptionsMenu(menu: Menu): Boolean {
-  //      menuInflater.inflate(R.menu.settings_menu, menu)
-  //      return true
-  //  }
+
+    private fun setUserData() {
+
+        val username = userViewModel.userState.value.name
+        val password = userViewModel.userState.value.passwd
+
+
+            activeUsernameTextView.text = username
+            activePasswordTextView.text = password
+    }
+
+
     fun callHome(v: View?) {
         startActivity(Intent(this@settings_page_activity, home_page_activity::class.java))
     }
+
+
     fun callLogout(v: View?) {
         startActivity(Intent(this@settings_page_activity, Welcome::class.java))
     }
